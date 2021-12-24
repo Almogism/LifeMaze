83ab79b91c50492d64a5fb580dc4e8eeac9c8ec7
alexsavizky <66139461+alexsavizky@users.noreply.github.com>
2021-12-24 13:37:08 +0200

    backend
    backend

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # LifeMaze
 The game of Life Maze.
+
+
+installed(for alex)
+npm i express mysql dotenv hbs
+npm i --save nodemon
+npm install ejs --save
+npm install path --save
---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mysql = require("mysql");
+var path = require('path');
+
+dotenv.config({path: './.env'});
+
+const app = express();
+//create conection to the data base (db)
+const db = mysql.createConnection({
+    host : process.env.host,
+    user: process.env.user,
+    password : process.env.password,
+    database : process.env.database
+});
+
+//connect 
+db.connect((error)=>{
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log("mysql connected :)");
+    }
+})
+
+
+/*
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+*/
+/*
+app.use(express.static(__dirname + '/public'));
+*/
+app.use(express.urlencoded({extended: false}));
+
+app.use(express.json());
+
+const publicDirectory = path.join(__dirname,'/public');
+app.use(express.static(publicDirectory));
+
+app.set('view engine', 'hbs');
+app.get('/',(req,res)=>{
+    res.render('firstpage');
+})
+
+app.get('/login',(req,res)=>{
+    res.render('login');
+})
+//local host - 3000
+app.listen(3000,()=>{
+    console.log("server started on port 3000");
+})
